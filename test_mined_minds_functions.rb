cf75cc83b6311011e2597a067d329aa5a5d00e59
Mike Pratt <michaelrpratt24@icloud.com>
2017-01-16 16:21:27 -0500

    New Function Subsituted In To Test

--- test_mined_minds_functions.rb
+++ test_mined_minds_functions.rb
@@ -1,8 +1,5 @@
 require 'minitest/autorun'
-
-def get_mined_minds_result(number)
-	1
-end
+require_relative 'mined_minds_functions.rb'
 
 class TestMMFunctions < Minitest::Test
 	def test_one_equals_one()
