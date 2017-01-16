ce6edf2314f0ed9a25d0c0684f8a0af655d8e035
Mike Pratt <michaelrpratt24@icloud.com>
2017-01-16 13:58:22 -0500

    Mined and Mind Numbers 1-100

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,5 +1,14 @@
 number = 1
-100.times do 
-	puts number
-	number = number + 1
+100.times do
+
+	if number == 3
+		puts "mined"
+		number = number + 1
+
+	else
+		puts number
+		number = number + 1
+
+	end
+	
 end	
