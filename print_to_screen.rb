ada3ecca26d7acd67fb09a07273624272638a46e
Mike Pratt <michaelrpratt24@icloud.com>
2017-01-16 14:02:21 -0500

    Code Refactoring

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,14 +1,9 @@
 number = 1
 100.times do
-
 	if number == 3
-		puts "mined"
-		number = number + 1
-
+		puts "mined"	
 	else
 		puts number
-		number = number + 1
-
 	end
-	
+	number = number + 1
 end	
